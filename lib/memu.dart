37381c786e7d3b895c693483d4cd9a16ade85471
minyeong-Kim207 <cyzhalsdud88@gmail.com>
2024-10-03 16:27:18 +0900

    bless

--- lib/memu.dart
+++ lib/memu.dart
@@ -20,38 +20,41 @@ class _MainMenuState extends State<MainMenu> {
       appBar: AppBar(title: Text('Main Menu')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // 여기에 페이지 전환만 설정
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CameraPage()),
-                );
-              },
-              child: Text('Scan (API 호출 테스트 중)'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // CropImagePage로 이동만 설정
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: (context) => UserInfo()),
-                );
-              },
-              child: Text('User Info (회원가입 페이지)'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context, 
-                  MaterialPageRoute(builder: (context) => signupPage()),
-                );
-              },
-              child: Text('회원 로그인 관리'),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // 여기에 페이지 전환만 설정
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CameraPage()),
+                  );
+                },
+                child: Text('Scan (API 호출 테스트 중)'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // CropImagePage로 이동만 설정
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(builder: (context) => UserInfo()),
+                  );
+                },
+                child: Text('User Info (회원가입 페이지)'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context, 
+                    MaterialPageRoute(builder: (context) => signupPage()),
+                  );
+                },
+                child: Text('회원 로그인 관리'),
+              ),
+            ],
+          ),
         ),
       ),
     );
